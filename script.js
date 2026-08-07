4a11f54b5ddd3ef6fff7801e0bccbcd7943216e9
Ebu3v <chukwudebeluchukwuebuka17@gmail.com>
2026-08-07 17:59:54 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// set current year
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector('.section-hero');
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    document.body.classList.add('sticky');
+  } else {
+    document.body.classList.remove('sticky');
+  }
+};
+
+const obs = new IntersectionObserver(stickyNav, {
+  //in the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`,
+});
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////
+/////Smooth scrolling animation
+
+document.querySelector('.nav__links').addEventListener('click', e => {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+});
+// close mobile navigation
+// if (link.classList.contains("main-nav-link"))
+// headerEl.classList.toggle("nav-open");
+
+//Reveal Section
+const allSection = document.querySelectorAll('.section');
+
+const revealSection = (entries, observer) => {
+  // console.log(entries);
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSection.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+/////////////////////////////////////////////////
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
